e6ed47caee497d112840f6270ba0c5cec259ef76
Ncognxto <c.hicks2929@gmail.com>
2021-06-20 16:14:50 -0500

    Initial commit

---
+++ src/main/java/org/launchcode/hellospring/controllers/HelloController.java
@@ -0,0 +1,79 @@
+package org.launchcode.hellospring.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
+@ResponseBody
+@RequestMapping("hello")
+public class HelloController {
+
+    // Handles request at path /hello
+//    @GetMapping("hello")
+//    @ResponseBody
+//    public String hello() {
+//        return "Hello, Spring!";
+//    }
+
+    // lives at /hello/goodbye
+    @GetMapping("goodbye")
+    public String goodbye() {
+        return "Goodbye, Spring!";
+    }
+
+    // lives at /hello/hello
+    //Handles request of the form /hello?name=LaunchCode
+    @RequestMapping(method = {RequestMethod.GET, RequestMethod.POST})
+    public String helloWithQueryParam(@RequestParam String name) {
+        return "Hello, " + name + "!";
+    }
+
+    //Handles requests of the form /hello/LaunchCode
+    @GetMapping("{name}")
+    public String helloWithPathParam(@PathVariable String name){
+        return "Hello, " + name + "!";
+    }
+
+    // lives at /hello/form
+    @GetMapping("form")
+    public String helloForm(){
+        return "<html>" +
+                "<body>" +
+                "<form action='/hello' method='post'>" + //submit a request to /hello
+                "<input type='text' name='name'>" +
+                "<input type='submit' value='Greet me!'>" +
+                "</form>" +
+                "</body>" +
+                "</html>";
+    }
+
+    @RequestMapping(value = "hello", method = RequestMethod.POST)
+    @ResponseBody
+    public String helloPost(@RequestParam String name, @RequestParam String language) {
+        if(name == null) {
+            name = "World!";
+        }
+        return createMessage(name, language);
+    }
+
+    public static String createMessage(String n, String l) {
+        String greeting = "";
+
+        if(l.equals("english")) {
+            greeting = "Hello";
+        }
+        else if (l.equals("french")) {
+            greeting = "Bonjour";
+        }
+        else if (l.equals("italian")) {
+            greeting = "Bonjourno";
+        }
+        else if (l.equals("spanish")) {
+            greeting = "Hola";
+        }
+        else if (l.equals("german")) {
+            greeting = "Hallo";
+        }
+        return greeting + " " + n;
+    }
+}
